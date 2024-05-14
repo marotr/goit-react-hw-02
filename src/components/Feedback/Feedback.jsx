eef5ca7a7c40f05606db8de7aecebbd50a166c66
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-05-14 08:41:54 +0300

    imports

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,9 @@
+import css from './Feedback.module.css'
+
+const Feedback = () => {
+  return (
+    <div className={css.container}>Feedback</div>
+  )
+}
+
+export default Feedback
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,9 @@
+import css from './Options.module.css'
+
+const Options = () => {
+  return (
+    <div className={css.container}>Options</div>
+  )
+}
+
+export default Options

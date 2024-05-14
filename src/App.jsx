06b2bee404ffa32a2e037d4f2806eb1fa1035891
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-05-14 08:37:31 +0300

    basic structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Description from './components/Description/Description'
+import Feedback from './components/Feedback/Feedback'
+import Options from './components/Options/Options'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Description />
+      <Feedback />
+      <Options />
+    </>
+  );
+};
+
+export default App
